7ed908836470b6435d8b10b11265a3a21bebcf8d
oficina91 <oficina102bogota@gmail.com>
2026-01-29 21:33:50 -0500

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <Header />
+  <nav class="nav">
+    <RouterLink to="/">Inicio</RouterLink>
+    <RouterLink to="/102-cagadas">102 Cagadas del petrismo</RouterLink>
+    <RouterLink to="/gastos-hormiga">Gastos Hormiga</RouterLink>
+    <RouterLink to="/mis-compromisos">Mis Compromisos</RouterLink>
+    <RouterLink to="/quien-soy">Quién Soy</RouterLink>
+    <RouterLink to="/unete">Únete</RouterLink>
+  </nav>
 
   <main>
     <RouterView />
@@ -11,7 +18,20 @@ import Header from './components/Header.vue'
 </script>
 
 <style scoped>
-main {
-  position: relative;
+.nav {
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+  background: #111;
+}
+
+.nav a {
+  color: white;
+  text-decoration: none;
+}
+
+.nav a.router-link-active {
+  font-weight: bold;
+  border-bottom: 2px solid white;
 }
 </style>
